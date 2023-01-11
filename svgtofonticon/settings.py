27bd7648c006b636e526a587bda41b2794ede343
ZabihullahNoorzai <zabihullah.noorzai786@gmail.com>
2023-01-11 10:37:19 -0800

    Railway Updates

--- svgtofonticon/settings.py
+++ svgtofonticon/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -140,6 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -150,6 +151,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 CRISPY_ALLOWED_TEMPLATE_PACKS ="bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
